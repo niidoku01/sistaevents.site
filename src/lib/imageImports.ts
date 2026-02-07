3e26c2b5fe5775cd8e06e28ef2d336dc200cc027
niidoku01 <niidoku01@gmail.com>
2026-02-07 04:09:44 +0000

    Fix all image paths for production deployment

---
+++ src/lib/imageImports.ts
@@ -0,0 +1,63 @@
+// Import all static images
+// Lights
+import light1 from "@/assets/lights/light 1.jpg";
+import light2 from "@/assets/lights/light 2.jpg";
+import light3 from "@/assets/lights/light 3.jpg";
+import light4 from "@/assets/lights/light 4.jpg";
+import light5 from "@/assets/lights/light 5.jpg";
+
+// Chairs
+import chair1 from "@/assets/chairs/chair 1.jpg";
+import chair2 from "@/assets/chairs/chair 2.jpg";
+import chair3 from "@/assets/chairs/chair 3.jpg";
+import chair4 from "@/assets/chairs/chair 4.jpg";
+import chair5 from "@/assets/chairs/chair 5.jpg";
+import chair6 from "@/assets/chairs/chair 6.jpg";
+
+// Tables
+import table2 from "@/assets/Table/table 2.jpg";
+import table3 from "@/assets/Table/table 3.jpg";
+
+// Tents
+import tent1 from "@/assets/tents/tent 1.jpg";
+import tent2 from "@/assets/tents/tent 2.jpg";
+import tent3 from "@/assets/tents/tent 3.jpg";
+import tent4 from "@/assets/tents/tent 4.jpg";
+
+// Flatware
+import flatware1 from "@/assets/flatware/flatware 1.jpg";
+import flatware2 from "@/assets/flatware/flatware 2.jpg";
+import flatware3 from "@/assets/flatware/flatware 3.jpg";
+import flatware4 from "@/assets/flatware/flatware 4.jpg";
+import flatware5 from "@/assets/flatware/flatware 5.jpg";
+
+// Others
+import other1 from "@/assets/others/other 1.jpg";
+import other2 from "@/assets/others/other 2.jpg";
+import other3 from "@/assets/others/other 3.jpg";
+import other4 from "@/assets/others/other 4.jpg";
+import other5 from "@/assets/others/other 5.jpg";
+import other6 from "@/assets/others/other 6.jpg";
+
+// Flowers
+import flower1 from "@/assets/flowers/flower 1.jpg";
+import flower2 from "@/assets/flowers/flower 2.jpg";
+import flower3 from "@/assets/flowers/flower 3.jpg";
+import flower4 from "@/assets/flowers/flower 4.jpg";
+import flower5 from "@/assets/flowers/flower 5.jpg";
+
+// Hero/About images
+import sistaEvents from "@/assets/sista-events.jpeg";
+import sabout from "@/assets/sabout.jpeg";
+import sistalogo from "@/assets/sistalogo.svg";
+
+export const images = {
+  lights: { light1, light2, light3, light4, light5 },
+  chairs: { chair1, chair2, chair3, chair4, chair5, chair6 },
+  tables: { table2, table3 },
+  tents: { tent1, tent2, tent3, tent4 },
+  flatware: { flatware1, flatware2, flatware3, flatware4, flatware5 },
+  others: { other1, other2, other3, other4, other5, other6 },
+  flowers: { flower1, flower2, flower3, flower4, flower5 },
+  misc: { sistaEvents, sabout, sistalogo },
+};
